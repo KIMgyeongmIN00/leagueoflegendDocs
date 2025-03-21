bc966af14da7ec354ccb17f49be0c9ceaa7f6271
KGyoeng <rlarudals61@naver.com>
2025-03-21 21:58:35 +0900

    test3

--- src/app/champions/api/fetch-champion-detail.ts
+++ src/app/champions/api/fetch-champion-detail.ts
@@ -7,7 +7,7 @@ export async function fetchChampionData(championId: string) {
   const version = await fetchLatestVersion();
 
   const response = await fetch(
-    `https://ddragon.leagueoflegends.com/cdn/${version}/data/ko_KR/champion/${championId}.json`
+    `https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/champion/${championId}.json`
   );
 
   const data: DetailResponse = await response.json();
